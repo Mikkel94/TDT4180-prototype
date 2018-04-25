c2ce1f4eadbcf54940cead4fcdd76679da08a74d
Runar Espelund <runar_espelund@outlook.com>
2018-04-25 13:30:31 +0200

    Oppdatert profil controller

--- src/oving5/ProfileController.java
+++ src/oving5/ProfileController.java
@@ -1,5 +1,53 @@
 package oving5;
 
-public class ProfileController extends MenuController {
-	
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+
+public class ProfilController extends MenuController {
+
+    @FXML
+    private Button home;
+
+    @FXML
+    private Button profile;
+
+    @FXML
+    private Button trophy;
+
+    @FXML
+    private Button konk;
+
+    @FXML
+    private Button btnNiva;
+
+    @FXML
+    private ToggleButton btnAnonym;
+
+    @FXML
+    private Button btnGodta;
+
+    @FXML
+    private Button btnAvsla;
+
+    @FXML
+    void goHomeAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goKonkAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goProfileAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goTrophyAction(ActionEvent event) {
+
+    }
+
 }
