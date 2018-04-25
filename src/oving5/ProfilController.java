dd43a62ca4f3cc430c5ce2cbb438e30549badb8a
Mikkel Sandsbråten <mikkel.sandsbraaten@gmail.com>
2018-04-25 14:45:04 +0200

    profil done, added store to use to store variables

--- src/oving5/ProfilController.java
+++ src/oving5/ProfilController.java
@@ -2,12 +2,62 @@ package oving5;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class ProfilController extends MenuController {
 
     @FXML private Button btnNiva, btnGodta, btnAvsla;
+    
+    @FXML private Label txtNiva, anon, invitations, lagnavn;
 
     @FXML private ToggleButton btnAnonym;
+    
+    @FXML private HBox teamInvite; 
+    
+    @FXML private VBox allTeamInvites;
+    
+    @FXML protected void initialize() {
+    		txtNiva.setText(Store.level);
+    		anon.setText("Anonym: " + String.valueOf(Store.isAnon));
+    		if (Store.myTeam.length() > 0) {
+    			godta();
+    		} else if (!Store.isInvitedToTeam) {
+    			avslag();
+    		}
+    }
+    
+    @FXML private void godta() {
+    		Store.isInvitedToTeam = false;
+    		Store.myTeam = lagnavn.getText();
+    		teamInvite.getChildren().clear();
+    		invitations.setText("Du er med i lag: " + Store.myTeam);
+    }
+    
+    @FXML private void avslag() {
+    		Store.isInvitedToTeam = false;
+    		teamInvite.getChildren().clear();
+    		invitations.setText("Ingen laginvitasjoner :(");
+    }
+    
+    @FXML private void byttAnon() {
+    		if (Store.isAnon == false) {
+    			Store.isAnon = true;
+    		} else {
+    			Store.isAnon = false;
+    		}
+    		anon.setText("Anonym: " + String.valueOf(Store.isAnon));
+    }
+    
+    @FXML private void byttNivaAction() {
+    		if (Store.level.equals("Nybegynner")) {
+    			Store.level = "Avansert";
+    		}else {
+    			Store.level = "Nybegynner";	
+    		}
+    		txtNiva.setText(Store.level);	
+    }
 
 }
---
+++ src/oving5/Store.java
@@ -0,0 +1,8 @@
+package oving5;
+
+public class Store {
+	public static String myTeam = "";
+	public static String level = "Nybegynner";
+	public static boolean isAnon = false;
+	public static boolean isInvitedToTeam = true;
+}
